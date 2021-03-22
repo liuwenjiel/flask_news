665e81804eb1bf10413478c461ca57e360a50668
wenjieliu <1494039400@qq.com>
2021-03-22 15:11:15 +0800

    个人中心--修改密码

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -72,6 +72,102 @@ def base_info():
     return jsonify(errno=RET.OK, errmsg="修改成功")
 
 
+# 获取/设置,用户头像上传
+# 请求路径: /user/pic_info
+# 请求方式:GET,POST
+# 请求参数:无, POST有参数,avatar
+# 返回值:GET请求: user_pci_info.html页面,data字典数据, POST请求: errno, errmsg,avatar_url
+@profile_blue.route('/pic_info', methods=['GET', 'POST'])
+@user_login_data
+def pic_info():
+    """
+    1. 判断请求方式,如果是get请求
+    2. 携带用户的数据,渲染页面
+    3. 如果是post请求
+    4. 获取参数
+    5. 校验参数,为空校验
+    6. 上传图像,判断图片是否上传成功
+    7. 将图片设置到用户对象
+    8. 返回响应
+    :return:
+    """
+    # 1. 判断请求方式,如果是get请求
+    if request.method == "GET":
+        # 2. 携带用户的数据,渲染页面
+        return render_template("news/user_pic_info.html", user_info=g.user.to_dict())
+
+    # 3. 如果是post请求
+    # 4. 获取参数
+    avatar = request.files.get("avatar")
+
+    # 5. 校验参数,为空校验
+    if not avatar:
+        return jsonify(errno=RET.PARAMERR, errmsg="图片不能为空")
+
+    # 6. 上传图像,判断图片是否上传成功
+    try:
+        # 读取图片为二进制,上传图片
+        image_name = image_storage(avatar.read())
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="七牛云异常")
+
+    if not image_name:
+        return jsonify(errno=RET.NODATA, errmsg="图片上传失败")
+
+    # 7. 将图片设置到用户对象
+    g.user.avatar_url = image_name
+
+    # 8. 返回响应
+    data = {
+        "avatar_url": constants.QINIU_DOMIN_PREFIX + image_name
+    }
+    return jsonify(errno=RET.OK, errmsg="上传成功", data=data)
+
+
+# 获取/设置用户密码
+# 请求路径: /user/pass_info
+# 请求方式:GET,POST
+# 请求参数:GET无, POST有参数,old_password, new_password
+# 返回值:GET请求: user_pass_info.html页面,data字典数据, POST请求: errno, errmsg
+@profile_blue.route('/pass_info', methods=['GET', 'POST'])
+@user_login_data
+def pass_info():
+    """
+    1. 判断请求方式,如果是get请求
+    2. 直接渲染页面
+    3. 如果是post请求,获取参数
+    4. 校验参数,为空校验
+    5. 判断老密码是否正确
+    6. 设置新密码
+    7. 返回响应
+    :return:
+    """
+    # 1. 判断请求方式,如果是get请求
+    if request.method == "GET":
+        # 2. 直接渲染页面
+        return render_template("news/user_pass_info.html")
+
+    # 3. 如果是post请求,获取参数
+    old_password = request.json.get("old_password")
+    new_password = request.json.get("new_password")
+
+    # 4. 校验参数,为空校验
+    if not all([old_password, new_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
+
+    # 5. 判断老密码是否正确
+    if not g.user.check_password(old_password):
+        return jsonify(errno=RET.DATAERR, errmsg="老密码错误")
+
+    # 6. 设置新密码
+    g.user.password = new_password
+
+    # 7. 返回响应
+    return jsonify(errno=RET.OK, errmsg="修改成功")
+
+
 # 获取/设置,新闻发布
 # 请求路径: /user/news_release
 # 请求方式:GET,POST
@@ -202,100 +298,3 @@ def collection():
         "news_list": news_list
     }
     return render_template("news/user_collection.html", data=data)
-
-
-# 获取/设置用户密码
-# 请求路径: /user/pass_info
-# 请求方式:GET,POST
-# 请求参数:GET无, POST有参数,old_password, new_password
-# 返回值:GET请求: user_pass_info.html页面,data字典数据, POST请求: errno, errmsg
-@profile_blue.route('/pass_info', methods=['GET', 'POST'])
-@user_login_data
-def pass_info():
-    """
-    1. 判断请求方式,如果是get请求
-    2. 直接渲染页面
-    3. 如果是post请求,获取参数
-    4. 校验参数,为空校验
-    5. 判断老密码是否正确
-    6. 设置新密码
-    7. 返回响应
-    :return:
-    """
-    # 1. 判断请求方式,如果是get请求
-    if request.method == "GET":
-        # 2. 直接渲染页面
-        return render_template("news/user_pass_info.html")
-
-    # 3. 如果是post请求,获取参数
-    old_password = request.json.get("old_password")
-    new_password = request.json.get("new_password")
-
-    # 4. 校验参数,为空校验
-    if not all([old_password, new_password]):
-        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
-
-    # 5. 判断老密码是否正确
-    if not g.user.check_password(old_password):
-        return jsonify(errno=RET.DATAERR, errmsg="老密码错误")
-
-    # 6. 设置新密码
-    g.user.password = new_password
-
-    # 7. 返回响应
-    return jsonify(errno=RET.OK, errmsg="修改成功")
-
-
-# 获取/设置,用户头像上传
-# 请求路径: /user/pic_info
-# 请求方式:GET,POST
-# 请求参数:无, POST有参数,avatar
-# 返回值:GET请求: user_pci_info.html页面,data字典数据, POST请求: errno, errmsg,avatar_url
-@profile_blue.route('/pic_info', methods=['GET', 'POST'])
-@user_login_data
-def pic_info():
-    """
-    1. 判断请求方式,如果是get请求
-    2. 携带用户的数据,渲染页面
-    3. 如果是post请求
-    4. 获取参数
-    5. 校验参数,为空校验
-    6. 上传图像,判断图片是否上传成功
-    7. 将图片设置到用户对象
-    8. 返回响应
-    :return:
-    """
-    # 1. 判断请求方式,如果是get请求
-    if request.method == "GET":
-        # 2. 携带用户的数据,渲染页面
-        return render_template("news/user_pic_info.html", user_info=g.user.to_dict())
-
-    # 3. 如果是post请求
-    # 4. 获取参数
-    avatar = request.files.get("avatar")
-
-    # 5. 校验参数,为空校验
-    if not avatar:
-        return jsonify(errno=RET.PARAMERR, errmsg="图片不能为空")
-
-    # 6. 上传图像,判断图片是否上传成功
-    try:
-        # 读取图片为二进制,上传图片
-        image_name = image_storage(avatar.read())
-
-    except Exception as e:
-        current_app.logger.error(e)
-        return jsonify(errno=RET.THIRDERR, errmsg="七牛云异常")
-
-    if not image_name:
-        return jsonify(errno=RET.NODATA, errmsg="图片上传失败")
-
-    # 7. 将图片设置到用户对象
-    g.user.avatar_url = image_name
-
-    # 8. 返回响应
-    data = {
-        "avatar_url": constants.QINIU_DOMIN_PREFIX + image_name
-    }
-    return jsonify(errno=RET.OK, errmsg="上传成功", data=data)
-
