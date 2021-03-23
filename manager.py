4b4104e7c2f23bb45f5cd9cd0e25f43d209546e1
wenjieliu <1494039400@qq.com>
2021-03-23 14:07:57 +0800

    管理员用户创建

--- manager.py
+++ manager.py
@@ -1,25 +1,59 @@
 """
-相关配置
+相关配置信息:
 1.数据库配置
 2.redis配置
 3.session配置
 4.csrf配置
 """
-from info import create_app, db, models  # 导入models的作用是让应用程序知道有这个东西的存在
+from flask import current_app
+from info import create_app, db, models  # 导入models的作用是让整个应用程序知道有models的存在
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from info.models import User
 
-# 调用方法，获取app
+# 调用方法,获取app
 app = create_app("develop")
 
-# 创建manager对象，管理app
+# 创建manager对象,管理app
 manager = Manager(app)
 
-# 使用Migrate关联app，db
+# 使用Migrate关联app,db
 Migrate(app, db)
 
 # 给manager添加一条操作命令
 manager.add_command("db", MigrateCommand)
+
+
+# 定义方法,创建管理员对象
+# @manager.option给manger添加一个脚本运行的方法
+# 参数1: 在调用方法的时候传递的参数名称
+# 参数2: 是对参数1的解释
+# 参数3: 目的参数,用来传递给形式参数使用的
+# ctrl + l 清空控制台
+# $ python manager.py --help
+# $ python manager.py create_superuser -?
+# $ python manager.py create_superuser -u admin -p 12345678
+@manager.option('-u', '--username', dest='username')
+@manager.option('-p', '--password', dest='password')
+def create_superuser(username, password):
+    # 1.创建用户对象
+    admin = User()
+
+    # 2.设置用户属性
+    admin.nick_name = username
+    admin.mobile = username
+    admin.password = password
+    admin.is_admin = True
+
+    # 3.保存到数据库
+    try:
+        db.session.add(admin)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        return "创建失败"
+
+    return "创建成功"
 
 
 if __name__ == '__main__':
